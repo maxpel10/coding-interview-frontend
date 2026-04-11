d96809c6ed7dfe6f2843ca7b7078fa64dab1e6d6
Maxi Pellegrino <mp@singularagency.co>
2026-04-11 13:26:58 -0300

    Currency model

---
+++ lib/features/exchange_calculator/models/currency.dart
@@ -0,0 +1,13 @@
+class Currency {
+  const Currency({
+    required this.id,
+    required this.symbol,
+    required this.name,
+    required this.assetPath,
+  });
+
+  final String id;
+  final String symbol;
+  final String name;
+  final String assetPath;
+}
