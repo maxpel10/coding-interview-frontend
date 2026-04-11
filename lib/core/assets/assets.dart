89b4691b597ecd3381d2388c77f591c7eab6d017
Maxi Pellegrino <mp@singularagency.co>
2026-04-11 13:25:25 -0300

    Assets configuration

---
+++ lib/core/assets/assets.dart
@@ -0,0 +1,10 @@
+class Assets {
+  static const String criptoCurrencies = 'assets/cripto_currencies';
+  static const String fiatCurrencies = 'assets/fiat_currencies';
+
+  static const String usdt = '$criptoCurrencies/TATUM-TRON-USDT.png';
+  static const String ves = '$fiatCurrencies/VES.png';
+  static const String pen = '$fiatCurrencies/PEN.png';
+  static const String brl = '$fiatCurrencies/BRL.png';
+  static const String cop = '$fiatCurrencies/COP.png';
+}
